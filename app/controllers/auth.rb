8c571568bef7c27e2673b7e07a9c8d0544083191
marizaswart <mariza.swart@gmail.com>
2017-11-23 18:49:29 +0100

    Create sign up and user params

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -15,4 +15,11 @@ class SlowFoodApp
     @users = User.all
     erb :users_index
   end
+
+  post '/register' do
+    user_params = params['user']
+    user = User.new(user_params)
+    user.save
+    redirect '/', notice: "Thank you for signing up #{user.name}"
+  end
 end
