8abeb2eceab1e8681e94d7b2f617a65f3e1c1bbb
Francesco <francesco.falchi@gmail.com>
2017-11-24 20:49:44 +0100

    Create last if statement, change postions, all green

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -18,13 +18,16 @@ class SlowFoodApp
 
   post '/register' do
     user_params = params['user']
+    user = User.new(user_params)
+    if user.save
+      redirect '/', notice: "Thank you for signing up #{user.name}"
+    end
     if user_params.any? {|k,v| v.empty? }
       redirect '/signup', error: "You need to add a name" if user_params['name'].empty?
       redirect '/signup', error: "You need to add a valid password" if user_params['password'].empty?
     end
-    user = User.new(user_params)
-    if user.save
-      redirect '/', notice: "Thank you for signing up #{user.name}"
+    if user_params['password'] != user_params['password_confirmation']
+      redirect '/signup', error: "You need to confirm your password"
     end
 
   end
