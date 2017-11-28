642b620471b554f777da60e806ec345a5be74ba2
marizaswart <mariza.swart@gmail.com>
2017-11-28 20:45:23 +0100

    Change on the seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 require 'faker'
-
+categories = ['starter','main course', 'dessert']
 10.times do
   Product.create(
       name: Faker::Food.dish,
-      category: Product::VALID_CATEGORIES.sample,
+      category: categories.sample,
       price: rand(10..100),
       description: Faker::Lorem.paragraph
   )
